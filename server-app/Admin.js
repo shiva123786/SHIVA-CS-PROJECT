c6d9fff9c02f5b3fe9278549d2cb67f275d31c56
kethavath shiva <ugs23114_aids.shiva@cbit.org.in>
2025-07-08 18:20:22 +0530

    Delete server-app directory

--- server-app/Admin.js
+++
@@ -1,9 +0,0 @@
-// Admin login schema
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  username: String,
-  password: String // NOTE: should be hashed in real projects
-});
-
-module.exports = mongoose.model('Admin', adminSchema);
